715365e8a83f718a439790dd9908f0c91da3fc17
vibzz2003 <500093004@stu.upes.ac.in>
2024-04-17 16:29:59 +0530

    initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosComponent } from "./MyComponents/todos/todos.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, TodosComponent]
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'todo-list-vibhor';
+  constructor(){
+    // setTimeout(() => {
+    //   this.title = "changed title";
+    // }, 2000);
+
+  }
 }
